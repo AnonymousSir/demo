be85dbfa9d29e90018381978dfc1e4e1838fcb24
AnonymousSir <58241148+AnonymousSir@users.noreply.github.com>
2022-01-02 15:22:29 +0800

    Delete login_token/server directory

--- login_token/server/router/index.js
+++
@@ -1,7 +0,0 @@
-const routers = {
-    login: require('./login'),
-    verifytoken: require('./verifytoken'),
-    list: require('./list'),
-    user: require('./user'),
-}
-module.exports = routers
